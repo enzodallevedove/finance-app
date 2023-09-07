522bdabec077e58e6997d5226105a95988560515
Enzo Dal Evedove <enzodallevedove@hotmail.com>
2023-09-07 20:32:41 +0000

    fixed api auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
             'password' => 'required|string',
         ]);
         $credentials = $request->only('email', 'password');
-        $token = Auth::attempt($credentials);
+        $token = Auth('api')->attempt($credentials);
 
         if (!$token) {
             return response()->json([
