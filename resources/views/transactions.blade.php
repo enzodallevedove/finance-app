8fa70120c9365d6964ea6da7ee0d86be1c2cd5e5
Enzo Dal Evedove <enzodallevedove@hotmail.com>
2023-09-08 23:32:18 +0000

    Improved data display formats

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -28,8 +28,8 @@
                             @foreach ($transactions as $transaction)
                                 <tr>
                                     <td class="wider-column"><h4>{{ $transaction->name }}</h4></td>
-                                    <td class="wider-column"><h4>{{ $transaction->value }}</h4></td>
-                                    <td class="wider-column"><h4>{{ date('H:i d-m-Y', strtotime($transaction->date)) }}</h4></td>
+                                    <td class="wider-column"><h4>{{ 'R$ ' . number_format($transaction->value, 2, ',', '.') }}</h4></td>
+                                    <td class="wider-column"><h4>{{ date('d/m/Y', strtotime($transaction->date)) }}</h4></td>
                                     <td class="crud-icons">
                                         <a href="{{ route('transactions.show', ['transaction' => $transaction->id]) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                                             {{ __('Details') }}
