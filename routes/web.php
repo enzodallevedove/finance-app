72971a23fe93dce1711f6a7369d436c2c58c3962
Enzo Dal Evedove <enzodallevedove@hotmail.com>
2023-09-08 01:26:48 +0000

    protected route

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('paymentoptions', PaymentOptionController::class);
-Route::resource('transactions', TransactionController::class);
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -32,6 +29,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('paymentoptions', PaymentOptionController::class);
+    Route::resource('transactions', TransactionController::class);
 });
 
 require __DIR__.'/auth.php';
