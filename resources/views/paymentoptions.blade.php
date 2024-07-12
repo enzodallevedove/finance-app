75271497af60c87e7f4f9dc9e41cc63d76ac17c7
Enzo Dal Evedove <enzodallevedove@hotmail.com>
2024-07-12 01:27:47 +0000

    feat: change colors based on balance

--- resources/views/paymentoptions.blade.php
+++ resources/views/paymentoptions.blade.php
@@ -10,7 +10,8 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="w-full flex justify-end mt-4 mb-8">
-                        <a href="{{ route('paymentoptions.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                        <a href="{{ route('paymentoptions.create') }}"
+                            class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                             {{ __('Add Payment Option') }}
                         </a>
                     </div>
@@ -29,19 +30,21 @@
                         </thead>
                         <tbody>
                             @foreach ($paymentOptions as $paymentOption)
-                            <tr>
-                                <td class="wider-column">
-                                    <h4>{{ $paymentOption->name }}</h4>
-                                </td>
-                                <td class="wider-column">
-                                    <h4>{{ 'R$ ' . number_format($paymentOption->balance, 2, ',', '.') }}</h4>
-                                </td>
-                                <td class="crud-icons">
-                                    <a href="{{ route('paymentoptions.show', ['paymentoption' => $paymentOption->id]) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                                        {{ __('Details') }}
-                                    </a>
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td class="wider-column">
+                                        <h4>{{ $paymentOption->name }}</h4>
+                                    </td>
+                                    <td class="wider-column">
+                                        <h4 @if ($paymentOption->balance > 0) style="color: green" @else style="color:red"
+                                        @endif>{{ 'R$ ' . number_format($paymentOption->balance, 2, ',', '.') }}</h4>
+                                    </td>
+                                    <td class="crud-icons">
+                                        <a href="{{ route('paymentoptions.show', ['paymentoption' => $paymentOption->id]) }}"
+                                            class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                                            {{ __('Details') }}
+                                        </a>
+                                    </td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
