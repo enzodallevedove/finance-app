6c11a8c7aa6a98568a11cb606bdbc0cffc9171f7
Enzo Dal Evedove <enzodallevedove@hotmail.com>
2024-07-13 19:03:04 +0000

    feat: Added payment option to transactions listing

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -20,6 +20,7 @@
                             <tr>
                                 <th><h3>Name</h3></th>
                                 <th><h3>Value</h3></th>
+                                <th><h3>Payment Option</h3></th>
                                 <th><h3>Date</h3></th>
                                 <th style="display: none">Actions</th>
                             </tr>
@@ -29,6 +30,7 @@
                                 <tr>
                                     <td class="wider-column"><h4>{{ $transaction->name }}</h4></td>
                                     <td class="wider-column" @if ($transaction->value > 0) style="color: green" @else style="color:red" @endif><h4>{{ 'R$ ' . number_format($transaction->value, 2, ',', '.') }}</h4></td>
+                                    <td class="wider-column"><h4>{{ $transaction->paymentOption->name }}</h4></td>
                                     <td class="wider-column"><h4>{{ date('d/m/Y', strtotime($transaction->date)) }}</h4></td>
                                     <td class="crud-icons">
                                         <a href="{{ route('transactions.show', ['transaction' => $transaction->id]) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
