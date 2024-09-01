3d3525e57514ee68a3cc511110b0711c3f038d59
Enzo Dal Evedove <enzodallevedove@hotmail.com>
2024-09-01 19:24:43 +0000

    fix: Broken html

--- resources/views/transactions/show.blade.php
+++ resources/views/transactions/show.blade.php
@@ -93,18 +93,6 @@
                         </div>
 
                         <div class="mt-4">
-                            <x-input-label for="paymentoption_id" :value="__('Payment Option')" />
-
-                            <select name="paymentoption_id" required
-                                class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
-                                @foreach ($paymentOptions as $paymentOption)
-                                <option value="{{ $paymentOption->id }}"
-                                    @selected($transaction->paymentoption_id==$paymentOption->id)>
-                                    {{ $paymentOption->name }}
-                                </option>
-                                @endforeach
-                            </select>
-
                             <div class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
                                 @foreach ($categories as $category)
                                     <span class="m-1 flex" style="line-height: 2rem">
