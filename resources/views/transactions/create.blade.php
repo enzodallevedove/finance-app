0b6f9e73eba17f6a6b62cd1837e9bd9e6308eddd
Enzo Dal Evedove <enzodallevedove@hotmail.com>
2025-02-22 01:35:17 +0000

    :sparkles: Created value input mask

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -27,7 +27,7 @@
                         <div class="mt-4">
                             <x-input-label for="value" :value="__('Value')" />
 
-                            <x-text-input id="value" type="number" name="value" step="0.01" required
+                            <x-text-input id="value" type="text" name="value" required
                                 autocomplete="value" class="block mt-1 w-full" />
 
                             <x-input-error :messages="$errors->get('value')" class="mt-2" />
@@ -89,4 +89,29 @@
             </div>
         </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#value').on('input', function(e) {
+                let value = $(this).val().replace(/\D/g, '');
+
+                if (!value) {
+                    $(this).val('0,00');
+                    return;
+                }
+
+                value = (parseInt(value) / 100).toFixed(2);
+
+                // Format with thousand separators and comma decimal
+                value = value.replace('.', ',');
+                value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+                $(this).val(value);
+            });
+
+            // Initialize with empty or existing value
+            $('#value').trigger('input');
+        });
+    </script>
 </x-app-layout>
